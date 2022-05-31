c28984509a65ee7eb27c96f2232f72c236f2fe7e
episohe <maybe_be@naver.com>
2022-05-31 09:46:46 +0900

    :zap: MeView =>2.7

--- users/serializers.py
+++ users/serializers.py
@@ -13,3 +13,14 @@ class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username", "first_name", "last_name", "email", "avatar", "superhost", "favs")
+
+
+class WriteUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email")
+
+    # def validated_data(self, value):
+    #     print(value)
+    #     return value.upper()
--- users/views.py
+++ users/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
-from users.serializers import ReadUserSerializer
+from users.serializers import *
 
 
 class MeView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        if request.user.is_authenticated:
-            return Response(ReadUserSerializer(request.user).data)
-        pass
+        return Response(ReadUserSerializer(request.user).data)
 
     def put(self, request):
-        pass
+        serializer = WriteUserSerializer(request.data, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
+# @permission_classes([IsAuthenticated])
 def user_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
