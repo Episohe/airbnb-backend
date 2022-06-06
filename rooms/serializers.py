a561398c801484139f761490b8cd26f498e2de69
episohe <maybe_be@naver.com>
2022-06-06 18:01:17 +0900

    :sparkles: Create Room Logic => 3.3

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-
-from rooms.models import Room
-from users.serializers import RelatesUserSerializer
+from users.serializers import UserSerializer
+from .models import Room
 
 
 class RoomSerializer(serializers.ModelSerializer):
-    user = RelatesUserSerializer()
+    user = UserSerializer(read_only=True)
+    is_fav = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -15,12 +15,12 @@ class RoomSerializer(serializers.ModelSerializer):
     def validate(self, data):
         if self.instance:
             check_in = data.get("check_in", self.instance.check_in)
-            check_out = data.get("check_in", self.instance.check_out)
+            check_out = data.get("check_out", self.instance.check_out)
         else:
-            check_in = data.get('check_in')
-            check_out = data.get('check_out')
-            if check_in == check_out:
-                raise serializers.ValidationError("Not enough time between changes")
+            check_in = data.get("check_in")
+            check_out = data.get("check_out")
+        if check_in == check_out:
+            raise serializers.ValidationError("Not enough time between changes")
         return data
 
     def get_is_fav(self, obj):
@@ -31,19 +31,7 @@ class RoomSerializer(serializers.ModelSerializer):
                 return obj in user.favs.all()
         return False
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.address = validated_data.get("address", instance.address)
-        instance.price = validated_data.get("price", instance.price)
-        instance.beds = validated_data.get("beds", instance.beds)
-        instance.lat = validated_data.get("lat", instance.lat)
-        instance.lng = validated_data.get("lng", instance.lng)
-        instance.bedrooms = validated_data.get("bedrooms", instance.bedrooms)
-        instance.bathrooms = validated_data.get("bathrooms", instance.bathrooms)
-        instance.check_in = validated_data.get("check_in", instance.check_in)
-        instance.check_out = validated_data.get("check_out", instance.check_out)
-        instance.instant_book = validated_data.get(
-            "instant_book", instance.instant_book
-        )
-        instance.save()
-        return instance
+    def create(self, validated_data):
+        request = self.context.get("request")
+        room = Room.objects.create(**validated_data, user=request.user)
+        return room
